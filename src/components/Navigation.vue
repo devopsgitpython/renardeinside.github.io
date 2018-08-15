c666d0a3e9399fdde8a2630574fda6582accb9f9
Трусов Иван Алексеевич <IATrusov@sberbank.ru>
2018-08-16 01:34:34 +0300

    initial

---
+++ src/components/Navigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-tabs 
+    slider-color="black"
+    centered
+  >
+        <v-tab to="/" router>About</v-tab>
+        <v-tab to="/posts" router>Posts</v-tab>
+        <v-tab to="/projects" router>Projects</v-tab>
+  </v-tabs>
+</template>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import { store } from './store'
+
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
